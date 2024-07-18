7e30be314e3e974b8429bbe3fa90d50065c20ad9
Anastasiia Komleva <asyakoma1998@gmail.com>
2024-07-18 18:29:36 +0300

    init: добавляет все карточки с профилями в блок

---
+++ my-app/src/components/blocks/profiles-list/profiles-list.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Card } from '../../types/card';
+
+import CardProfile from "../../ui/card-profile/card-profile";
+import './profiles-list.modules.scss'
+
+type CardsProfileProps = {
+  cards: Card[] | null;
+};
+
+function ProfilesList({cards}: CardsProfileProps): JSX.Element {
+    return (
+      <section className="profiles-list">
+        {cards?.length ? (
+          <>
+            <ul className="profiles-list__list">
+              {cards?.map((card) => <CardProfile card={card} key={card.id} />)}
+            </ul>
+          </>
+        ) : null}
+      </section>
+    );
+  }
+  
+  export default ProfilesList;
