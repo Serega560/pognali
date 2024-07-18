88d554eeafa20b7c64496730f8b09c0c56d546d7
Anastasiia Komleva <asyakoma1998@gmail.com>
2024-07-18 23:43:19 +0300

    init: добавляет блоки в каталог

--- my-app/src/components/pages/catalog/catalog.tsx
+++ my-app/src/components/pages/catalog/catalog.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ProfilesList from "../../blocks/profiles-list/profiles-list";
 import cardList from '../../../mocks/cardList';
+import BtnShowMore from '../../ui/buttons/btn-show-more/btn-show-more';
+import Pagination from '../../blocks/pagination/pagination';
 import "./catalog.module.scss"
 
 
 function Catalog(): JSX.Element {
     return (
       <>
-        <ProfilesList cards={cardList} />
+        <div className="catalog__wrapper">
+          <ProfilesList cards={cardList} />
+          <div className="catalog__show-more">
+            <BtnShowMore />
+          </div>
+          <Pagination />
+        </div>
       </>
     );
   }
