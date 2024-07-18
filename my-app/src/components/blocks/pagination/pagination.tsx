b5a719fcbb99c42da3a359259ab16224fd56f1a1
Anastasiia Komleva <asyakoma1998@gmail.com>
2024-07-18 23:42:05 +0300

    init: добавляет пагинацию

---
+++ my-app/src/components/blocks/pagination/pagination.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './pagination.module.scss'
+
+function Pagination() {
+    return (
+      <section className="pagination">
+        <ul className="pagination__page-list">
+            <li className="pagination__page-item active">
+                <span>1</span>
+            </li>
+            <li className="pagination__page-item">
+                <span>2</span>
+            </li>
+            <li className="pagination__page-item">
+                <span>3</span>
+            </li>
+            <li className="pagination__page-item">
+                <span>4</span>
+            </li>
+        </ul>
+
+        <div className="pagination__arrows">
+            <button className="pagination__arrow pagination__arrow--prev">
+                ←
+            </button>
+            <button className="pagination__arrow pagination__arrow--next">
+                →
+            </button>
+        </div>
+        
+      </section>
+    );
+  }
+  
+  export default Pagination;
