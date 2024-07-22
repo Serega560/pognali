6788f3d9423a9b09e01c4846e52bc8474f72ba5a
Anastasiia Komleva <asyakoma1998@gmail.com>
2024-07-23 00:21:55 +0300

    init: добавляет фильтры в разметку на страницу

--- my-app/src/components/pages/catalog/catalog.tsx
+++ my-app/src/components/pages/catalog/catalog.tsx
@@ -3,6 +3,7 @@ import ProfilesList from "../../blocks/profiles-list/profiles-list";
 import cardList from '../../../mocks/cardList';
 import BtnShowMore from '../../ui/buttons/btn-show-more/btn-show-more';
 import Pagination from '../../blocks/pagination/pagination';
+import Filters from '../../blocks/filters/filters';
 import "./catalog.module.scss"
 
 
@@ -10,11 +11,16 @@ function Catalog(): JSX.Element {
     return (
       <>
         <div className="catalog__wrapper">
-          <ProfilesList cardsData={cardList} />
-          <div className="catalog__show-more">
-            <BtnShowMore />
+          <div className="catalog__filters">
+            <Filters />
           </div>
-          <Pagination />
+          <div className="catalog__inner-wrapper">
+            <ProfilesList cardsData={cardList} />
+            <div className="catalog__show-more">
+              <BtnShowMore />
+            </div>
+            <Pagination />
+          </div>
         </div>
       </>
     );
