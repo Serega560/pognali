26686b0e2f79313ff82e868f8da06e2813b7083b
Anastasiia Komleva <asyakoma1998@gmail.com>
2024-07-24 02:56:08 +0300

    init: добавляет и стилизует направления

---
+++ my-app/src/components/blocks/directions-profile/directions-profile.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Level from '../../ui/level/level';
+import InputHashtags from '../../ui/input-hashtags/input-hashtags';
+import InputTransport from '../../ui/input-transport/input-transport';
+import './directions-profile.module.scss'
+
+function DirectionsProfile() {
+    return (
+      <section className="directions-profile">
+        <div className="directions-profile__container container">
+          <div className="directions-profile__wrapper">
+            <div className="directions-profile__level">
+              <Level />
+            </div>
+            <div className="directions-profile__image">
+              <picture className="directions-profile__figure">
+                <img 
+                  src="" 
+                  alt="" 
+                />
+              </picture>
+            </div>
+            <div className="directions-profile__upload-image">
+              <button className="directions-profile__upload-image-btn" type="button">сменить фото</button>
+            </div>
+            <div className="directions-profile__hashtags">
+              <InputHashtags />
+            </div>
+            <div className="directions-profile__transport">
+              <InputTransport />
+            </div>
+          </div>
+        </div>
+        
+      </section>
+    );
+  }
+  
+  export default DirectionsProfile;
