9f02921113ec07fcdc2c4470d0a054f820c6da79
Anastasiia Komleva <asyakoma1998@gmail.com>
2024-07-24 02:57:03 +0300

    fix: изменяет каталог

--- my-app/src/components/pages/catalog/catalog.tsx
+++ my-app/src/components/pages/catalog/catalog.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Heading from '../../blocks/heading/heading';
 import ProfilesList from "../../blocks/profiles-list/profiles-list";
 import cardList from '../../../mocks/cardList';
 import BtnShowMore from '../../ui/buttons/btn-show-more/btn-show-more';
@@ -9,20 +10,21 @@ import "./catalog.module.scss"
 
 function Catalog(): JSX.Element {
     return (
-      <>
-        <div className="catalog__wrapper">
-          <div className="catalog__filters">
+      <section className="catalog">
+        <Heading />
+        <div className="catalog__wrapper container">
+          <section className="catalog__filters">
             <Filters />
-          </div>
-          <div className="catalog__inner-wrapper">
+          </section>
+          <section className="catalog__inner-wrapper">
             <ProfilesList cardsData={cardList} />
             <div className="catalog__show-more">
               <BtnShowMore />
             </div>
             <Pagination />
-          </div>
+          </section>
         </div>
-      </>
+      </section>
     );
   }
   
