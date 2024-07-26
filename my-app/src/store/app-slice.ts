7fc3fb542417b65d6675f63f3061ff0cf5d21293
Anastasiia Komleva <asyakoma1998@gmail.com>
2024-07-26 19:58:55 +0300

    fix: добавляет континенты

--- my-app/src/store/app-slice.ts
+++ my-app/src/store/app-slice.ts
@@ -3,57 +3,62 @@ import { NameSpace } from '../const';
 // import { addDays } from 'date-fns';
 
 export type DateState = {
-    startDate: Date;
-    endDate: Date;
-    key: string;
-}
+  startDate: Date;
+  endDate: Date;
+  key: string;
+};
 
 export type AppSlice = {
-//  companions: number;
-//  duration: number;
-//  dateState: DateState[];
- choosenLetter: string;
- isSelectCountryInputActive: boolean;
-}
+  // companions: number;
+  // duration: number;
+  // dateState: DateState[];
+  choosenLetter: string;
+  choosenContinent: string; // Добавляем состояние для выбранного континента
+  isSelectCountryInputActive: boolean;
+};
 
 const initialState: AppSlice = {
-    // companions: 1,
-    // duration: 2,
-    // dateState: [
-    //     {
-    //       startDate: new Date(),
-    //       endDate: addDays(new Date(), 1),
-    //       key: 'selection'
-    //     }
-    //   ],
-    choosenLetter: 'А',
-    isSelectCountryInputActive: false,
+  // companions: 1,
+  // duration: 2,
+  // dateState: [
+  //   {
+  //     startDate: new Date(),
+  //     endDate: addDays(new Date(), 1),
+  //     key: 'selection',
+  //   },
+  // ],
+  choosenLetter: 'А',
+  choosenContinent: 'Европа', // Инициализируем выбранный континент
+  isSelectCountryInputActive: false,
 };
 
 export const slice = {
-    app: NameSpace.appSlice
-}
+  app: NameSpace.appSlice,
+};
 
 export const appSlice = createSlice({
   name: slice.app,
   initialState,
   reducers: {
-  //  setCompanions: (state, action: PayloadAction<number>) => {
-  //   state.companions = action.payload;
-  //  },
-  //  setDuration: (state, action: PayloadAction<number>) => {
-  //   state.duration = action.payload;
-  //  },
-  //  setDateState: (state, action: PayloadAction<DateState[]>) => {
-  //   state.dateState = action.payload;
-  //  },
-   setLetter: (state, action: PayloadAction<string>) => {
-    state.choosenLetter = action.payload;
-   },
-   setIsSelectCountryInputActive: (state) => {
-    state.isSelectCountryInputActive = !state.isSelectCountryInputActive;
-   }
-  }
+    // setCompanions: (state, action: PayloadAction<number>) => {
+    //   state.companions = action.payload;
+    // },
+    // setDuration: (state, action: PayloadAction<number>) => {
+    //   state.duration = action.payload;
+    // },
+    // setDateState: (state, action: PayloadAction<DateState[]>) => {
+    //   state.dateState = action.payload;
+    // },
+    setLetter: (state, action: PayloadAction<string>) => {
+      state.choosenLetter = action.payload;
+    },
+    setContinent: (state, action: PayloadAction<string>) => { // Добавляем редуктор для установки выбранного континента
+      state.choosenContinent = action.payload;
+    },
+    setIsSelectCountryInputActive: (state) => {
+      state.isSelectCountryInputActive = !state.isSelectCountryInputActive;
+    },
+  },
 });
 
 export const {
@@ -61,5 +66,6 @@ export const {
     // setDateState,
     // setDuration,
     setLetter,
+    setContinent,
     setIsSelectCountryInputActive,
 } = appSlice.actions;
