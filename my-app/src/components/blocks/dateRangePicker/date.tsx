668936e3f0094a0e62d09490d131a611d8d94aca
Anastasiia Komleva <asyakoma1998@gmail.com>
2024-07-29 18:34:01 +0300

    fix: измененения для дата

--- my-app/src/components/blocks/dateRangePicker/date.tsx
+++ my-app/src/components/blocks/dateRangePicker/date.tsx
@@ -1,31 +1,31 @@
 
 import React, { useState } from 'react';
-import Flatpickr from 'react-flatpickr';
-import { Options } from 'flatpickr/dist/types/options';
-import 'flatpickr/dist/flatpickr.min.css';
-import 'flatpickr/dist/l10n/ru.js';
-import './DateRangePicker.css';
+// import Flatpickr from 'react-flatpickr';
+// import { Options } from 'flatpickr/dist/types/options';
+// import 'flatpickr/dist/flatpickr.min.css';
+// import 'flatpickr/dist/l10n/ru.js';
+// import './DateRangePicker.css';
 
-const DateRangePicker: React.FC = () => {
-   const [dateRange, setDateRange] = useState<Date[]>([]);
+// const DateRangePicker: React.FC = () => {
+//    const [dateRange, setDateRange] = useState<Date[]>([]);
 
-   const options: Options = {
-      mode: 'range',
-      dateFormat: 'Y-m-d',
-      locale: 'ru',
-      static: true,
-      inline: true,
-   };
+//    const options: Options = {
+//       mode: 'range',
+//       dateFormat: 'Y-m-d',
+//       locale: 'ru',
+//       static: true,
+//       inline: true,
+//    };
 
-   return (
-      <div className="date-range-picker-container">
-         <Flatpickr
-            options={options}
-            value={dateRange}
-            onChange={(selectedDates: Date[]) => setDateRange(selectedDates)}
-         />
-      </div>
-   );
-}
+//    return (
+//       <div className="date-range-picker-container">
+//          <Flatpickr
+//             options={options}
+//             value={dateRange}
+//             onChange={(selectedDates: Date[]) => setDateRange(selectedDates)}
+//          />
+//       </div>
+//    );
+// }
 
-export default DateRangePicker;
+// export default DateRangePicker;
