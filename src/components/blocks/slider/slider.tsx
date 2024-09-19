540d3a33be71bea119179578ce9ed1fce7737ae7
Vladimir Yaschuk <vova.yashchuk.88@mail.ru>
2024-09-20 00:36:14 +0300

    feat: правит баги в слайдере

--- src/components/blocks/slider/slider.tsx
+++ src/components/blocks/slider/slider.tsx
@@ -14,12 +14,25 @@ const RangeSlider = () => {
     <div className="slider__holder">
       <div className="slider__range">
         <input type="number"
-          onChange={(evt) => setRange((current) => [Number(evt.target.value), current[1]])}
+          onChange={(evt) => setRange((current) => [(Number(evt.target.value) > current[1] ? current[1] - 1 : Number(evt.target.value)), current[1]])}
           value={range[0]}
+          max={range[1]}
+          onKeyDown={(evt) => {
+            if (evt.key === ',' || evt.key === '.') {
+              evt.preventDefault();
+            }
+          }}
         />
         <input type="number"
           onChange={(evt) => setRange((current) => [current[0], Number(evt.target.value)])}
+          onBlur={(evt) => setRange((current) => [current[0], (Number(evt.target.value) < current[0] ? current[0] + 1 : Number(evt.target.value))])}
           value={range[1]}
+          min={range[0]}
+          onKeyDown={(evt) => {
+            if (evt.key === ',' || evt.key === '.') {
+              evt.preventDefault();
+            }
+          }}
         />
       </div>
       <Slider
